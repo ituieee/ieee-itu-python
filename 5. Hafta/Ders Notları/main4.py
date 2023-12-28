23730bd166eb7423a62447280909ba4dd2a8a023
Adf qsa <theturksbot@proton.me>
2023-12-28 15:22:14 +0300

    Ders İçeriğinin Güncellenmesi

---
+++ 5. Hafta/Ders Notları/main4.py
@@ -0,0 +1,6 @@
+from okul_islemleri import * # Her şeyi çağırıyoruz
+
+print(siniflar) # Değişkene erişmek
+okul_yazdir("İstanbul Teknik Üniversitesi") # Fonksiyona erişmek
+ogrenci1 = Ogrenci("Umur", 3) # Sınıfa erişmek
+ogrenci1.info() # Bu objenin bir methodunu çağırmak
